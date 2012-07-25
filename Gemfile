b0f8883af5343cf79b7b14017611ff0ea3e81dd4
Mo Shahin <mshahin@softmills.com>
2012-07-25 12:15:13 -0400

    Done

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.6'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.10.0'
+  gem 'guard-rspec', '0.5.5'
 end
 
 
@@ -23,6 +24,12 @@ gem 'jquery-rails','2.0.2'
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'rb-fchange', '0.0.5'
+  gem 'rb-notifu', '0.0.4'
+  gem 'win32console', '1.3.0'
+
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,15 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
   
-    it "should have the right title" do
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should have the custome title" do
   		visit '/static_pages/home'
-  		page.should have_selector('title', 
+  		page.should_not have_selector('title', 
   						:text => " | Home")
 	end
 
